d90b7bbec999d450176a15f09e79a40b64a3ba4f
StelioJ <stelio.j@protonmail.com>
2021-09-17 23:57:06 +1000

    pdf view layout and view

--- BDViewerBlProto/Data/Models/CommonObjs.cs
+++ BDViewerBlProto/Data/Models/CommonObjs.cs
@@ -9,14 +9,30 @@ namespace BDViewerBlProto.Data.Models
     public class Acronym
     {
         public string AcronymAbbr { get; set; }
-        public string Definition { get; set; }        
+        public string Definition { get; set; }
+
+        public Acronym (string acronymAbbr, string definition)
+        {
+            AcronymAbbr = acronymAbbr;
+            Definition = definition;
+        }
     }
 
     public class RefDocuments
     {
+        public int RefId { get; set; }
         public string RefCode { get; set; }
         public string Title { get; set; }
         public string Date { get; set; }
         public string FileName { get; set; }
+
+        public RefDocuments(int refid, string refcode, string title, string date, string fileName)
+        {
+            RefId = refid;
+            RefCode = refcode;
+            Title = title;
+            Date = date;
+            FileName = fileName;
+        }
     }
 }
